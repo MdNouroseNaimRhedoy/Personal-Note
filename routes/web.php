67090ab9c5e4c1eadff8d21dfdb7cc0abc9776f8
Md. Nourose Naim Rhedoy <mdnourosenaimrhedoy@gmail.com>
2024-08-14 12:28:32 +0600

    feat: generate factory,resource controller, resource route and create seed data

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,18 @@
 <?php
 
+use App\Http\Controllers\NoteController;
 use App\Http\Controllers\WelcomeController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', [WelcomeController::class,'welcome'])->name('welcome');
+
+
+//Route::get('/note', [NoteController::class,'index'])->name('note.index');
+//Route::get('/note/create', [NoteController::class,'create'])->name('note.create');
+//Route::post('/note', [NoteController::class,'store'])->name('note.store');
+//Route::get('/note/{id}', [NoteController::class,'show'])->name('note.show');
+//Route::put('/note/{id}/edit', [NoteController::class, 'edit'])->name('note.edit');
+//Route::delete('/note/{id}', [NoteController::class,'destroy'])->name('note.destroy');
+
+// 7 lines in 1 line
+Route::resource('note',NoteController::class);
